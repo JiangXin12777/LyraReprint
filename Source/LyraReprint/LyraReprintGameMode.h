9669d4a043c37928207088551c97691d9fc013e0
WIN-D8SD5M7581S\Administrator <2649340476@qq.com>
2023-07-31 13:20:38 +0800

    bulid: new project

---
+++ Source/LyraReprint/LyraReprintGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LyraReprintGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ALyraReprintGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ALyraReprintGameMode();
+};
+
+
+
